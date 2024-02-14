55e93e3f5d799ac86ec57a6a01f7eac7e483ccbb
Tarje Lavik <tarje.lavik@gmail.com>
2024-02-14 15:36:28 +0100

    reset and create mvp starter

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Landsloven</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/uib-bub/exhibition-landsloven-nextra',
   },
-  chat: {
-    link: 'https://discord.com',
+  i18n: [
+    { locale: 'no', text: 'Nynorsk' },
+    { locale: 'en', text: 'Engelsk' },
+  ],
+  search: {
+    placeholder: 'Søk',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/uib-bub/ex-landsloven-nextrahi',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Universitetsbiblioteket i Bergen, Høyskolen på Vestlandet, Vestland fylkeskommune',
   },
 }
 
