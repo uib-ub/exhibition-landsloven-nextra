46825005df29e7437af12f8f980a26261743c2b7
Tarje Lavik <tarje.lavik@gmail.com>
2024-05-21 11:42:49 +0200

    Refactor alle pages to use new structure and add main pages component

--- components/main-pages.tsx
+++ components/main-pages.tsx
@@ -2,19 +2,23 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Meta } from 'nextra';
 
-const SubPages = ({ meta }: { meta: Meta }) => {
-  const subPages = Object.entries(meta).map(([key, value], i) => {
-    const title = typeof value === 'string' ? value : value.title
-    const path = key
-    const type = value?.type === 'separator'
+type MetaMenuItems = {
+  [key: string]: {
+    title: string;
+    href: string;
+  };
+};
 
-    // This is stupid, but since we get the links from _meta.json it gets messy. Should be a own config file.
-    if (i < 2 || type) return null
+const MainPages = ({ meta }: { meta: Meta }) => {
+  const items = meta['bolkene'].items;
+  const subPages = Object.entries(items as MetaMenuItems).map(([key, value], i) => {
+    const title = value.title
+    const path = value.href
 
     return (
       <div
         key={path}
-        className='flex flex-row gap-3 p-4 border border-gray-200'
+        className='flex flex-row gap-3 p-4 border border-red-500'
       >
         <Image src={'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMDAgMjAwIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCI+CiAgPHJlY3Qgd2lkdGg9IjIwMCIgaGVpZ2h0PSIyMDAiIGZpbGw9IiNjY2NjY2MiPjwvcmVjdD4KICA8dGV4dCB4PSI1MCUiIHk9IjUwJSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSIgdGV4dC1hbmNob3I9Im1pZGRsZSIgZm9udC1mYW1pbHk9Im1vbm9zcGFjZSIgZm9udC1zaXplPSIyNnB4IiBmaWxsPSIjMzMzMzMzIj4yMDB4MjAwPC90ZXh0PiAgIAo8L3N2Zz4='} alt={value.title} width={200} height={200} />
         <Link
@@ -38,4 +42,4 @@ const SubPages = ({ meta }: { meta: Meta }) => {
   );
 }
 
-export default SubPages;
+export default MainPages;
