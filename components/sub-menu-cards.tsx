b59dedf869f8717ac5e81a31e6a79f7bb1b07879
Tarje Lavik <tarje.lavik@gmail.com>
2024-05-29 20:18:05 +0200

    Update some example components

--- components/sub-menu-cards.tsx
+++ components/sub-menu-cards.tsx
@@ -27,17 +27,17 @@ const SubMenuCards = ({ items }: { items: MenuItems }) => {
     return (
       <div
         key={path}
-        className='flex flex-row gap-3 p-4 border border-red-500'
+        className='flex flex-row gap-3 p-3 bg-ll-blue'
       >
         <Image src={'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMDAgMjAwIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCI+CiAgPHJlY3Qgd2lkdGg9IjIwMCIgaGVpZ2h0PSIyMDAiIGZpbGw9IiNjY2NjY2MiPjwvcmVjdD4KICA8dGV4dCB4PSI1MCUiIHk9IjUwJSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSIgdGV4dC1hbmNob3I9Im1pZGRsZSIgZm9udC1mYW1pbHk9Im1vbm9zcGFjZSIgZm9udC1zaXplPSIyNnB4IiBmaWxsPSIjMzMzMzMzIj4yMDB4MjAwPC90ZXh0PiAgIAo8L3N2Zz4='} alt={value.title} width={200} height={200} />
-        <div className='flex flex-col gap-3'>
+        <div className='flex flex-col gap-3 p-3'>
           <Link
+            className='text-ll-gold md:text-2xl'
             href={path}
-            className='font-antiqua nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]'
           >
             {title}
           </Link>
-          {value.ingress ? <p>{value.ingress[locale]}</p> : null}
+          {value.ingress ? <p className='text-white'>{value.ingress[locale]}</p> : null}
         </div>
       </div>
     )
@@ -45,7 +45,7 @@ const SubMenuCards = ({ items }: { items: MenuItems }) => {
 
   return (
     <section className='w-full py-5'>
-      <div className='grid grid-cols-2 gap-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
         {subPages.map((subpage: any) => (
           subpage
         ))}
