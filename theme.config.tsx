5e8d386fc850c4c09691cb588e55d9f76a853272
Tarje Lavik <tarje.lavik@gmail.com>
2024-05-29 20:17:27 +0200

    Add fonts and increase base font size

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
-    <div className='flex gap-3'>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-        <circle cx="12" cy="12" r="12" />
-      </svg>
-      <span className='font-antiqua'>Landsloven</span>
+    <div className='flex gap-4'>
+      <div className='text-ll-red'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+          <circle cx="12" cy="12" r="12" />
+        </svg>
+      </div>
+      <span className=''>Landsloven</span>
     </div>
   ),
   i18n: [
@@ -18,8 +20,20 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/uib-ub/exhibition-landsloven-nextra',
   footer: {
-    text: 'Universitetsbiblioteket i Bergen, Høyskolen på Vestlandet, Vestland fylkeskommune',
+    text: (
+      <div className='font-serif'>
+        <p>Universitetsbiblioteket i Bergen, Høyskolen på Vestlandet, Vestland fylkeskommune</p>
+      </div>
+    ),
   },
+  /* primaryHue: {
+    dark: 200,
+    light: 200,
+  },
+  primarySaturation: {
+    dark: 50,
+    light: 50,
+  }, */
 }
 
 export default config
