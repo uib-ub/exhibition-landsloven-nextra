de7fc88c26b9e58512a165aba448feba479e4a71
Espen Moe-Nilssen <espen@medialog.no>
2024-06-05 20:05:02 +0200

    Design alternatives / negative logos / components

--- components/flexgrid.tsx
+++ components/flexgrid.tsx
@@ -21,19 +21,23 @@ const FlexGrid: React.FC<FlexGridProps> = (props) => {
   const { margin = 3, maxcolumns=6,  gap=3, padding=0, bgcolor='bg-white', bgimage='', children } = props;
 
   // Calculate responsive column counts
-  const columns = 'grid-cols-'  + Math.max(2, Math.floor(maxcolumns * 0.25)); 
-  const smcolumns = 'sm:grid-cols-' + Math.max(2, Math.floor(maxcolumns * 0.40));  
-  //const smcolumns = 'sm:grid-cols-1';
-  const mdcolumns = 'md:grid-cols-' + Math.max(2, Math.floor(maxcolumns * 0.5));  
-  const lgcolumns = 'lg:grid-cols-'  + Math.max(3, Math.floor(maxcolumns * 0.75));  
-  const xlcolumns = 'xl:grid-cols-' + maxcolumns;
-  const xxccolumns = 'xxl:grid-cols-' + maxcolumns;
+  // const columns = 'grid-cols-'  + Math.max(2, Math.floor(maxcolumns * 0.25)); 
+  const smcolumns = 'sm:grid-cols-' + Math.max(2, Math.floor(maxcolumns * 0.75));  
+  // //const smcolumns = 'sm:grid-cols-1';
+  // const mdcolumns = 'md:grid-cols-' + Math.max(2, Math.floor(maxcolumns * 0.5));  
+  // const lgcolumns = 'lg:grid-cols-'  + Math.max(3, Math.floor(maxcolumns * 0.75));  
+  // const xlcolumns = 'xl:grid-cols-' + maxcolumns;
+  // const xxccolumns = 'xxl:grid-cols-' + maxcolumns;
+
+  const columns = 'grid-cols-'  + Math.max(2, Math.floor(maxcolumns * 0.33)); 
+  const mdcolumns = 'md:grid-cols-${maxcolumns}' 
+
 
   //NOTE: To set for lower than 640 px I needed to set grid-cols-1
 
   return ( 
       <div 
-        className={`flex grid gap-${gap} ${columns} ${smcolumns} ${mdcolumns} ${lgcolumns} ${xlcolumns} ${xxccolumns}  p-${padding} my-${margin} ${bgcolor}`}
+        className={`flex gap-${gap} ${columns} ${smcolumns} ${mdcolumns} p-${padding} my-${margin} ${bgcolor}`}
          >
         {children} 
       </div>
--- components/sitathero.tsx
+++ components/sitathero.tsx
@@ -1,15 +1,28 @@
 import Image from 'next/image';
 import Container from 'components/container'
 
-const SitatHero = ({ title, ingress, footer }: { title?: string, ingress?: string, footer?: string }) => {
+const SitatHero = ({ title, ingress, footer, logo  }: { title?: string, ingress?: string, footer?: string, logo?: boolean }) => {
   return (
  
-      <div className="flex bg-ll-gold-50 -mx-6 dm:m-0 mb-3 p-8 relative left-0 right-0 min-h-[32vw] sm:min-h-[25vw]  bg-[url('/images/banner_overlay_transp.png')] sm:bg-[url('/images/banner_overlay.png')] sm:bg-contain bg-no-repeat bg-left">
+      <div className={`flex has-logo-${logo} bg-ll-gold-50 -mx-6 dm:m-0 mb-3 p-8 relative left-0 right-0 min-h-[32vw] sm:min-h-[25vw]  bg-[url('/images/banner_overlay_transp.png')] sm:bg-[url('/images/banner_overlay.png')] sm:bg-contain bg-no-repeat bg-left`}>
         <Container>
-        <div className="flex flex-wrap float-right bg-ll-gold-50 sm:pl-4 md:bg-none md:pl-0 sm:w-3/5 max-w-4xl h-auto">
-            <h4 className='text-red-700 nx-mt-2 text-2xl sm:text-4xl dark:text-red-700 font-medium mb-2'>{title}</h4>
+        <div className={`flex flex-wrap float-right sm:bg-ll-gold-50 sm:pl-4 md:bg-none md:pl-0 sm:w-3/5 max-w-4xl h-auto center-text-${logo}`}>
+            { logo && (
+               <Image 
+                    className="py-0  px-4  -mt-6 mb-5" 
+                    src="/images/logo_horizontal.svg" 
+                    alt="Landsloven logo" 
+                    width={550} 
+                    height={100}  />
+            )}
+           
+
+
+            <h4 className='text-red-700 w-full nx-mt-2 text-2xl sm:text-4xl dark:text-red-700 font-medium mb-2'>{title}</h4>
             <p className="text-black text-xl sm:text-3xl font-light">{ingress}</p>
-            <p className="w-full text-right mt-1 text-sm sm:text-base pt-2 pb-12"> <cite className="text-base sm:text-lg my-3 italic text-gray-700 dark:text-black">{footer}</cite></p> 
+            {footer && (
+              <p className="w-full text-right mt-1 text-sm sm:text-base pt-2 pb-12"> <cite className="text-base sm:text-lg my-3 italic text-gray-700 dark:text-black">{footer}</cite></p> 
+            )}            
          </div>
          </Container>
     
