f266568deae0c49eb03627e559cf44e6c1682d07
Espen Moe-Nilssen <espen@medialog.no>
2024-06-06 19:19:41 +0200

    Make sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  // siteUrl: process.env.SITE_URL || 'http://ubuntu.local:3000/',
+  siteUrl: process.env.SITE_URL || 'https://exhibition-landsloven-nextra.vercel.app/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
+
+
