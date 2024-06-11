773c765743f0800bd2987a036c615b73d49aff9c
Espen Moe-Nilssen <espen@medialog.no>
2024-06-11 15:24:52 +0200

    CSS fix

--- components/nextbreadcrumb.jsx
+++ components/nextbreadcrumb.jsx
@@ -33,8 +33,7 @@ const Breadcrumbs = () => {
   const lang  = router === 'en' ? 'en' : 'no'
   
   return (
-    <nav aria-label="breadcrumb" className="border-b xbg-ll-gold-50 py-1 px-2 nextra-breadcrumb nx-mt-1.5 nx-flex nx-items-center nx-gap-1 nx-overflow-hidden text-sm nx-text-gray-500 dark:nx-text-gray-400 contrast-more:nx-text-current">
-        
+    <nav aria-label="breadcrumb" className="border-b xbg-ll-gold-50 py-1 px-2  nextra-breadcrumb nx-mt-1.5 nx-flex nx-items-center nx-gap-1 nx-overflow-hidden text-sm nx-text-gray-500 dark:nx-text-gray-400 contrast-more:nx-text-current">
        <div className="flex flex-row flex-wrap text-normal p-2">
   
         <Link className="-ml-4" href="/">
@@ -45,15 +44,16 @@ const Breadcrumbs = () => {
             onClick={toggleVisibility}          >
            
             {/* <MenuIcon className="mx-1 nx-w-4.5 nx-shrink-0 path-black svg-black stoke-black hover:stroke-ll-blue-700 hover:rotate-180" />   */}
-            <ArrowRightIcon className="mx-1 nx-w-3.5 nx-shrink-0 hover:stroke-ll-blue-700 hover:rotate-90" /> 
+            <ArrowRightIcon className="h-5 mx-1 nx-w-3.5 nx-shrink-0 hover:stroke-ll-blue-700 hover:rotate-90" /> 
         </div>
          
 
-        <div aria-label="dropdown for sections" id="dropdown" className={`px-2 py-4 absolute xtop-0 z-10 ${isVisible ? 'opacity-100 h-auto' : 'opacity-0 h-1 pointer-events-none'} bg-ll-sandy-100 divide-y divide-gray-100 shadow-2xl w-auto dark:bg-gray-700`}
+        <div aria-label="dropdown for sections" id="dropdown" className={`px-2 py-4 h-3 absolute xtop-0 z-10 ${isVisible ? 'opacity-100 h-auto' : 'opacity-0 h-1 pointer-events-none'} bg-ll-sandy-100 divide-y divide-gray-100 shadow-2xl w-auto dark:bg-gray-700`}
                onClick={toggleVisibility}     >
             <div className="cursor-pointer pt-2 px-1 right align-right float-right" 
             onClick={toggleVisibility} >Close ✕</div>  
             <div className="px-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+              console.log('hallo')
                 {Object.keys(items).map(key => (
                       <Link key={key} className="block px-4 py-0 my-2 hover:text-ll-blue-600 dark:hover:bg-gray-600 dark:hover:text-white text-ll-blue-700 text-base  hover:underline" href={items[key].href}>{items[key].title[lang]}</Link>  
                 ))}
@@ -70,7 +70,7 @@ const Breadcrumbs = () => {
               <div 
                   className="flex flex-row flex-wrap cursor-pointer  px-1" 
                   onClick={toggleVisibility2} >
-                  <ArrowRightIcon className="hover:bg-ll-blue-200 mx-1 block nx-w-3.5 nx-shrink-0 hover:stroke-ll-blue-700 hover:rotate-90" />  
+                  <ArrowRightIcon className="h-5 hover:bg-ll-blue-200 mx-1 block nx-w-3.5 nx-shrink-0 hover:stroke-ll-blue-700 hover:rotate-90" />  
                   <div aria-current="page" className="text-[15px] font-sans ">{breadcrumb.label}</div>
               </div> 
             )
--- hooks/usebreadcrumbs.jsx
+++ hooks/usebreadcrumbs.jsx
@@ -7,7 +7,7 @@ import site from 'config/site';
 
 const useBreadcrumbs = () => {
   const router = useRouter(); 
-  const lang  = router === 'en' ? 'en' : 'no'
+  const lang  = router == 'en' ? 'en' : 'no'
 
   const pathSegments = router.pathname.split('/').filter(segment => segment);
 
@@ -15,15 +15,11 @@ const useBreadcrumbs = () => {
     var href = '/' + pathSegments.slice(0, index + 1).join('/').replace(/\.(no|en)$/, '');
     var seg = segment.replace(/\.(no|en)$/, '')  ;
     var label = seg
-    // var seksjon = site
-    var folder = 'xxx'
-    // console.log(router)
+    var folder = ''
 
     if (index == 0 ) {
-      label = site?.bolkene?.items[seg]?.title[lang]  ?? router === 'Folder' ? 'en' : 'Folder'
+      label = site?.bolkene?.items[seg]?.title[lang]  ?? '…'
       href =  site?.bolkene?.items[seg]?.href ?? '';
-      // seksjon = site.bolkene.items[seg] ?? '';
-      // return { href, label, index, key: seg+label+index  };
       
     }
 
@@ -31,7 +27,7 @@ const useBreadcrumbs = () => {
     if (index == 1 ) {
       // get title by looking it up from 'combinedsections'
       const bolk = pathSegments.slice(0, 1)[0];
-      label = sections[bolk]?.items[label].title[lang] ?? router === '…' ? 'en' : '…'
+      label = sections[bolk]?.items[label].title[lang] ??   '…'
       folder = sections[bolk]?.items;
       // return { href, label, index, folder: sections[bolk].items, key: seg+label+index  };
      }
