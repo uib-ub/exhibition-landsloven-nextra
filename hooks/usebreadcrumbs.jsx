b8a7e33f67070c5c21f42242799d9ee04c6d1bbb
Tarje Lavik <tarje.lavik@gmail.com>
2024-06-17 10:46:48 +0200

    To lowercase

--- hooks/usebreadcrumbs.jsx
+++ hooks/usebreadcrumbs.jsx
@@ -1,22 +1,38 @@
+import sections from 'components/combindesections';
 import site from 'config/site';
 import { useRouter } from 'next/router';
 
+
 const useBreadcrumbs = () => {
   const router = useRouter();
+  const lang = router == 'en' ? 'en' : 'no'
+
   const pathSegments = router.pathname.split('/').filter(segment => segment);
 
   return pathSegments.map((segment, index) => {
-    let href = '/' + pathSegments.slice(0, index + 1).join('/').replace(/\.(no|en)$/, '');
-    let label = segment.replace(/\.(no|en)$/, '');
+    var href = '/' + pathSegments.slice(0, index + 1).join('/').replace(/\.(no|en)$/, '');
+    var seg = segment.replace(/\.(no|en)$/, '');
+    var label = seg
+    var folder = ''
 
     if (index == 0) {
-      href = site.bolkene.items[label].href;
-      label = site.bolkene.items[label].title.no
+      label = site?.bolkene?.items[seg]?.title[lang] ?? '…'
+      href = site?.bolkene?.items[seg]?.href ?? '';
+
     }
 
-    //TO DO: Check language if we use the title
-    return { href, label, index };
+
+    if (index == 1) {
+      // get title by looking it up from 'combinedsections'
+      const bolk = pathSegments.slice(0, 1)[0];
+      label = sections[bolk]?.items[label]?.title[lang] ?? '…'
+      folder = sections[bolk]?.items;
+      // return { href, label, index, folder: sections[bolk].items, key: seg+label+index  };
+    }
+    return { href, label, index, folder, key: seg + label + index };
   });
 };
 
 export default useBreadcrumbs;
+
+
