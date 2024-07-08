3414400e77e724874ef94cfa28b3219458a186fa
Espen Moe-Nilssen <espen@medialog.no>
2024-07-08 12:46:42 +0200

    change Work height based on browser window  size (change if less than 768)

--- components/work.tsx
+++ components/work.tsx
@@ -1,7 +1,7 @@
 import { CloverViewerProps } from '@samvera/clover-iiif';
 import dynamic from "next/dynamic";
 import { useRouter } from 'next/router';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 const Viewer = dynamic(
   () => import("@samvera/clover-iiif").then((Clover) => Clover.Viewer),
@@ -13,19 +13,35 @@ const Viewer = dynamic(
 const API_URL = "https://api-ub.vercel.app"; // @TODO, set to prod API when it is ready
 
 const Work = ({ children, id, url, marcus, config }: { children?: ReactNode, id?: string, url?: string, marcus?: string, config?: CloverViewerProps }) => {
-  const manifestId = id ? `${API_URL}/items/${id}?as=iiif` : url
-  const { locale } = useRouter()
+  const manifestId = id ? `${API_URL}/items/${id}?as=iiif` : url;
+  const { locale } = useRouter();
 
-  const buttonText = locale === 'no' ? 'Se mer i Marcus' : 'Read more in Marcus'
+  const buttonText = locale === 'no' ? 'Se mer i Marcus' : 'Read more in Marcus';
+
+  const [canvasHeight, setCanvasHeight] = useState('600px');
+
+  useEffect(() => {
+    const updateCanvasHeight = () => {
+      if (window.innerWidth < 768) {
+        setCanvasHeight('380px'); // set your desired height for small screens
+      } else {
+        setCanvasHeight('auto'); // default height for larger screens
+      }
+    };
+
+    updateCanvasHeight(); // set initial value
+    window.addEventListener('resize', updateCanvasHeight); // update on resize
+
+    return () => window.removeEventListener('resize', updateCanvasHeight); // cleanup on unmount
+  }, []);
 
   return (
-    <div className='my-10 w-full flex flex-col lg:flex-row  bg-ll-sandy border-solid border-2 border-ll-sandy-100 dark:bg-ll-blue-900'>
+    <div className='my-10 w-full  h-[630px] md:h-[700px] lg:h-[70vh] flex flex-col lg:flex-row  bg-ll-sandy border-solid border-2 border-ll-sandy-100 dark:bg-ll-blue-900'>
       <div className='w-full relative dark:bg-ll-blue-950 z-0'>
         <Viewer
           iiifContent={manifestId}
           options={{
-            canvasHeight: '600px',  // @TODO: Better to Hard coded unitl bug (see line below) is fixed
-            //canvasHeight: '100%', // @TODO: Height bug reported to samvera (https://github.com/samvera-labs/clover-iiif/issues/214)
+            canvasHeight: canvasHeight,
             informationPanel: {
               open: false
             },
@@ -34,11 +50,11 @@ const Work = ({ children, id, url, marcus, config }: { children?: ReactNode, id?
         />
       </div>
       {children ?
-        <div className='p-5 sm:p-10 flex flex-col justify-between w-full lg:max-w-md  bg-ll-blue-900 text-white dark:nx-bg-primary-400/10'>
+        <div className='p-5 sm:p-10 flex flex-col justify-between w-full lg:max-w-md bg-ll-blue-900 text-white dark:nx-bg-primary-400/10'>
           {children}
           {marcus ?
             <a
-              className='rounded-full self-end px-5 py-2 m-5  bg-ll-red dark:bg-red-700'
+              className='rounded-full self-end px-5 py-2 m-5 bg-ll-red dark:bg-red-700'
               href={marcus}>
               {buttonText}
             </a>
