ea1313bc1936ea0bc51b06da5c4b03faaf464626
Tarje Lavik <tarje.lavik@gmail.com>
2024-09-06 13:55:18 +0200

    Preliminary footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,47 @@
-import HVLen from 'components/logos/hvl-en';
 import Bymuseet from 'components/logos/logo-bymuseet';
-import HVL from 'components/logos/logo-hvl';
 import UiBen from 'components/logos/logo-uib-en';
 import UiB from 'components/logos/logo-uib-no';
+import Vestland from 'components/logos/logo-vestland';
 import { useRouter } from 'next/router';
-//import Vestland from 'components/logos/logo-vestland';
+// import HVLen from 'components/logos/hvl-en';
+// import Hvl from 'components/logos/logo-hvl';
 
 const Footer = () => {
   const { locale } = useRouter();
   return (
-    <div className='flex flex-col justify-center sm:flex-row gap-5 w-full items-center space-x-2'>
-      {locale === 'no' ?
-        <>
-          <UiB className='flex-0 w-1/3 px-10' />
-          <HVL className='w-1/5' />
-          <Bymuseet className='flex-0 w-1/3 max-h-20' />
-          {/* <Vestland className='' /> */}
-        </>
-        :
-        <>
-          <UiBen className='flex-0 w-1/3 px-10' />
-          <HVLen className='flex-0 flex-shrink-0 w-1/5' />
-          <Bymuseet className='flex-0 w-1/3 max-h-20' />
-          {/* <Vestland className='' /> */}
-        </>
-      }
+    <div className='w-full'>
+      <div className='flex-grow mb-5 w-full'>
+        <p className='font-medium text-center'>Utstillingen er skapt av:</p>
+      </div>
+      <div className='flex flex-col sm:flex-row gap-20 w-full'>
+        {locale === 'no' ?
+          <>
+            <div className='w-1/3'>
+              <UiB />
+            </div>
+            <div className='w-1/3'>
+              <Bymuseet className='px-10' />
+            </div>
+            <div className='w-1/3'>
+              <Vestland className='px-10' />
+            </div>
+            {/* <Hvl className='w-1/5' /> */}
+          </>
+          :
+          <>
+            <div className='w-1/3'>
+              <UiBen />
+            </div>
+            <div className='w-1/3'>
+              <Bymuseet className='px-10' />
+            </div>
+            <div className='w-1/3'>
+              <Vestland className='px-10' />
+            </div>
+            {/* <HVLen className='flex-0 flex-shrink-0 w-1/5' /> */}
+          </>
+        }
+      </div>
     </div>
   );
 };
